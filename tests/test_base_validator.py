016cc104870bc9517f293866c082fbab74290cb2
Alex Parmentier <a.g.parmentier@gmail.com>
2016-07-20 15:03:11 -0400

    Refactor: Pulled out some testing methods.
    Hopefully these will be useful for writing tests for
    the individual IIIF resource testers.

--- tests/test_base_validator.py
+++ tests/test_base_validator.py
@@ -1,102 +1,119 @@
-import inspect
-import unittest
-
+from tests.validator_tester import ValidatorTestingTools
 from tripoli import IIIFValidator
 from tripoli.iiif_resource_validators.base_validator import BaseValidator
 
 
-class TestBaseValidatorMixin(unittest.TestCase):
+class TestBaseValidatorMixin(ValidatorTestingTools):
 
     def setUp(self):
         iv = IIIFValidator()
         iv.fail_fast = False
-        self.base_validator = BaseValidator(iv)
+        iv.collect_errors = True
+        iv.collect_warnings = True
+        iv.debug = False
+        self.test_subject = BaseValidator(iv)
 
-    def has_error(self, error):
-        """Check if a particular error was raised."""
-        return error in self.base_validator._errors
+    def test_compare_dicts(self):
+        man = {"field_1": "value", "field_3": "value"}
 
-    def has_errors(self):
-        return bool(self.base_validator._errors)
+        # Test that nothing is changed in valid case.
+        schema = {"field_1": self.fake_valid, "field_2": self.fake_valid}
+        val = self.test_subject._compare_dicts(schema, man)
+        self.assertFalse(self.has_errors())
+        self.assertEqual(man, val)
+        self.clear_errors_and_warnings()
 
-    def has_warning(self, warning):
-        return warning in self.base_validator._warnings
+        # Test that warning is logged in invalid case.
+        schema = {"field_1": self.fake_invalid, "field_2": self.fake_valid}
+        val = self.test_subject._compare_dicts(schema, man)
+        self.assertEqual(man, val)
+        self.assertTrue(self.has_errors())
+        self.clear_errors_and_warnings()
 
-    def has_warnings(self):
-        return bool(self.base_validator._warnings)
+        # Test that making a change does not affect original argument.
+        schema = {"field_1": self.fake_change}
+        old_man = man.copy()
+        val = self.test_subject._compare_dicts(schema, man)
+        self.assertEqual(old_man, man)
+        self.assertNotEqual(val, man)
 
-    def has_warnings_or_errors(self):
-        return not (bool(self.base_validator._warnings) or bool(self.base_validator._errors))
+    def check_recommended_fields(self):
+        # Test a warning comes through when missing a recommended field.
+        self.test_subject._check_recommended_fields("test", {}, ["test_field"])
+        self.assertTrue(self.has_warnings())
+        self.clear_errors_and_warnings()
 
-    def clear_errors(self):
-        self.base_validator._errors = set()
+        # Test no warning when all recommended fields are there.
+        self.test_subject._check_recommended_fields("test", {"test_field": "value"}, ["test_field"])
+        self.assertFalse(self.has_warnings())
 
-    def clear_warnings(self):
-        self.base_validator._warnings = set()
+    def check_unknown_fields(self):
+        # Test a warning comes through when an unknown field is included.
+        self.test_subject._check_unknown_fields("test", {"other_field": "value"}, ["test_field"])
+        self.assertTrue(self.has_warnings())
+        self.clear_errors_and_warnings()
 
-    def clear_errors_and_warnings(self):
-        self.base_validator._errors = set()
-        self.base_validator._warnings = set()
+        # Test no warning when all fields are known.
+        self.test_subject._check_unknown_fields("test", {"test_field": "value"}, ["test_field"])
+        self.assertFalse(self.has_warnings())
 
-    def assert_no_errors_with_inputs(self, fn, inputs):
-        """Run validation fn on each input, asserting no error is logged."""
-        argc = len(inspect.signature(fn).parameters)
-        for arg in inputs:
-            val = fn('unknown_field', arg) if argc >= 2 else fn(arg)
-            try:
-                self.assertFalse(self.has_errors(), "{} is not valid input".format(arg))
-                self.assertEqual(arg, val)
-            finally:
-                self.clear_errors()
+    def check_forbidden_fields(self):
+        # Test an error comes through when forbidden field is included.
+        self.test_subject._check_forbidden_fields("test", {"test_field": "value"}, ["test_field"])
+        self.assertTrue(self.has_errors())
+        self.clear_errors_and_warnings()
 
-    def assert_errors_with_inputs(self, fn, inputs):
-        """Run validation fn on each input, asserting each logs an error."""
-        argc = len(inspect.signature(fn).parameters)
-        for arg in inputs:
-            val = fn('unknown_field', arg) if argc >= 2 else fn(arg)
-            try:
-                self.assertTrue(self.has_errors(), "{} is valid input".format(arg))
-                self.assertEqual(arg, val)
-            finally:
-                self.clear_errors()
+        # Test no error when no fields are forbidden.
+        self.test_subject._check_forbidden_fields("test", {"other_field": "value"}, ["test_field"])
+        self.assertFalse(self.has_errors())
+
+    def check_required_fields(self):
+        # Test an error comes through when a required field is missing.
+        self.test_subject._check_required_fields("test", {"other_field": "value"}, ["test_field"])
+        self.assertTrue(self.has_errors())
+        self.clear_errors_and_warnings()
+
+        # Test no error when all required fields are included.
+        self.test_subject._check_required_fields("test", {"test_field": "value"}, ["test_field"])
+        self.assertFalse(self.has_errors())
 
     def test_str_or_val_lang_type(self):
         """Allows stings, lists of strings, val/lang dicts, and lists of val/lang dicts"""
         valid_inputs = ['hello', ['hello', 'there'], {'@language': 'en', '@value': 'hello'},
                         [{'@language': 'en', '@value': 'hello'}, {'@language': 'fr', '@value': 'bonjour'}]]
-        self.assert_no_errors_with_inputs(self.base_validator._str_or_val_lang_type, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject._str_or_val_lang_type, valid_inputs)
 
         invalid_inputs = [0, {'@value': 14, '@language': 'en'}, {'@value': 'Hello'}]
-        self.assert_errors_with_inputs(self.base_validator._str_or_val_lang_type, invalid_inputs)
+        self.assert_errors_with_inputs(self.test_subject._str_or_val_lang_type, invalid_inputs)
 
     def test_repeatable_str_type(self):
         """Allows strings or lists of strings."""
         valid_inputs = ['hello', ['hello', 'there']]
-        self.assert_no_errors_with_inputs(self.base_validator._repeatable_string_type, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject._repeatable_string_type, valid_inputs)
 
     def test_repeatable_uri_type(self):
         """Allow a single uri or list of uris (in both uri formats)"""
         valid_inputs = ['http://google.ca', ['http://google.com', {'@id': 'http://bing.com'}]]
-        self.assert_no_errors_with_inputs(self.base_validator._repeatable_uri_type, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject._repeatable_uri_type, valid_inputs)
 
         invalid_inputs = ['hello', ['hello', 'everyone']]
-        self.assert_errors_with_inputs(self.base_validator._repeatable_uri_type, invalid_inputs)
+        self.assert_errors_with_inputs(self.test_subject._repeatable_uri_type, invalid_inputs)
 
     def test_http_uri_type(self):
         """Allow a single uri in either format that must be http/s"""
         valid_inputs = ['http://google.ca', {'@id': 'https://bing.com'}]
-        self.assert_no_errors_with_inputs(self.base_validator._http_uri_type, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject._http_uri_type, valid_inputs)
 
         invalid_inputs = ['ftp://google.ca', ['http://google.ca', 'http://bing.ca']]
-        self.assert_errors_with_inputs(self.base_validator._http_uri_type, invalid_inputs)
+        self.assert_errors_with_inputs(self.test_subject._http_uri_type, invalid_inputs)
 
     def test_uri_type(self):
         """Allow a single uri in either format."""
         valid_inputs = ['http://google.ca', 'ftp://google.ca', {'@id': 'http://google.ca'}]
-        self.assert_no_errors_with_inputs(self.base_validator._uri_type, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject._uri_type, valid_inputs)
 
         invalid_inputs = [{'key': 'http:google.ca'}, 'hello', ['http://google.ca']]
-        self.assert_errors_with_inputs(self.base_validator._uri_type, invalid_inputs)
+        self.assert_errors_with_inputs(self.test_subject._uri_type, invalid_inputs)
 
     def test_metadata_field(self):
         """Allow properly formatted metadata as specified by the presentation api."""
@@ -105,7 +122,7 @@ class TestBaseValidatorMixin(unittest.TestCase):
             [{'value': [{'@language': 'en', '@value': 'hello'}], 'label': 'greeting'}],
             [{'value': 'hello', 'label': {'@language': 'en', '@value': 'greeting'}}],
         ]
-        self.assert_no_errors_with_inputs(self.base_validator.metadata_field, valid_inputs)
+        self.assert_no_errors_with_inputs(self.test_subject.metadata_field, valid_inputs)
 
         invalid_inputs = [
             [{'bad_key': 'wow', 'this_sucks': 'yup'}],
@@ -113,25 +130,31 @@ class TestBaseValidatorMixin(unittest.TestCase):
             "a stringier string",
             [{"value": "missing label"}]
         ]
-        self.assert_errors_with_inputs(self.base_validator.metadata_field, invalid_inputs)
+        self.assert_errors_with_inputs(self.test_subject.metadata_field, invalid_inputs)
 
     def test_general_image_resource(self):
         """Test correct behaviour of general image resource validation."""
         val = "http://google.ca"
-        self.base_validator._general_image_resource("unknown_field", val)
+        self.test_subject._general_image_resource("unknown_field", val)
         self.assertTrue(self.has_warnings())
         self.assertFalse(self.has_errors())
         self.clear_errors_and_warnings()
 
         val = {"@id": "http://google.ca"}
-        self.base_validator._general_image_resource("unknown_field", val)
+        self.test_subject._general_image_resource("unknown_field", val)
         self.assertTrue(self.has_warnings())
         self.assertFalse(self.has_errors())
         self.clear_errors_and_warnings()
 
         val = {"service": {"@context": "http://iiif.io/api/image/2/context.json"}}
-        self.base_validator._general_image_resource("unknown_field", val)
+        self.test_subject._general_image_resource("unknown_field", val)
         self.assertFalse(self.has_warnings())
         self.assertFalse(self.has_errors())
 
-
+    def test_view_dir_hint_field(self):
+        self.test_subject.VIEW_HINTS = {"paged", "non-paged"}
+        self.test_subject.VIEW_DIRS = {"paged", "non-paged"}
+        self.assert_no_errors_with_inputs(self.test_subject.viewing_hint_field, ["paged", "non-paged"])
+        self.assert_errors_with_inputs(self.test_subject.viewing_hint_field, ["error"])
+        self.assert_no_errors_with_inputs(self.test_subject.viewing_dir_field, ["paged", "non-paged"])
+        self.assert_errors_with_inputs(self.test_subject.viewing_dir_field, ["error"])
---
+++ tests/validator_tester.py
@@ -0,0 +1,65 @@
+import unittest
+import inspect
+
+class ValidatorTestingTools(unittest.TestCase):
+
+    def setUp(self):
+        self.test_subject = None
+        raise NotImplemented
+
+    def has_error(self, error):
+        """Check if a particular error was raised."""
+        return error in self.test_subject._errors
+
+    def has_errors(self):
+        return bool(self.test_subject._errors)
+
+    def has_warning(self, warning):
+        return warning in self.test_subject._warnings
+
+    def has_warnings(self):
+        return bool(self.test_subject._warnings)
+
+    def has_warnings_or_errors(self):
+        return not (bool(self.test_subject._warnings) or bool(self.test_subject._errors))
+
+    def clear_errors(self):
+        self.test_subject._errors = set()
+
+    def clear_warnings(self):
+        self.test_subject._warnings = set()
+
+    def clear_errors_and_warnings(self):
+        self.test_subject._errors = set()
+        self.test_subject._warnings = set()
+
+    def assert_no_errors_with_inputs(self, fn, inputs):
+        """Run validation fn on each input, asserting no error is logged."""
+        args = inspect.signature(fn).parameters
+        for arg in inputs:
+            val = fn('unknown_field', arg) if "field" in args else fn(arg)
+            try:
+                self.assertFalse(self.has_errors(), "{} is not valid input".format(arg))
+                self.assertEqual(arg, val)
+            finally:
+                self.clear_errors()
+
+    def assert_errors_with_inputs(self, fn, inputs):
+        args = inspect.signature(fn).parameters
+        for arg in inputs:
+            val = fn('unknown_field', arg) if "field" in args else fn(arg)
+            try:
+                self.assertTrue(self.has_errors(), "{} is valid input".format(arg))
+                self.assertEqual(arg, val)
+            finally:
+                self.clear_errors()
+
+    def fake_valid(self, value):
+        return value
+
+    def fake_invalid(self, value):
+        self.test_subject.log_error("fake_field", "test error")
+        return value
+
+    def fake_change(self, value):
+        return str(value) + "changed stuff"
