3724c6efc38178e1772cbafb58866027e2dcc3e4
Alex Parmentier <a.g.parmentier@gmail.com>
2016-07-25 09:57:57 -0400

    Docs: Version 1.0.1
    Need to update version for pypi. Only minor changes
    to warning behaviour made.

--- tripoli/tripoli.py
+++ tripoli/tripoli.py
@@ -28,7 +28,7 @@ from .resource_validators import (
     ManifestValidator, SequenceValidator, CanvasValidator,
     ImageContentValidator, AnnotationValidator)
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 
 class IIIFValidator(SubValidationMixin):
