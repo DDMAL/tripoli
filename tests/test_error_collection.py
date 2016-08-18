6eb4dbe53dacb0c7b597ac2569541593e07424fa
Alex Parmentier <a.g.parmentier@gmail.com>
2016-08-18 14:41:14 -0400

    New: Added IIIF test collection to tests.
    Each of these manifests is supposed to fail. Currently this
    is not the case, but including it in the standard tests gives
    us a good target.

---
+++ tests/test_error_collection.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+from .validator_testing_tools import ValidatorTestingTools
+from tripoli import IIIFValidator
+
+
+class TestErrorCollection(ValidatorTestingTools):
+
+    def setUp(self):
+        self.test_subject = IIIFValidator()
+        self.test_subject.logger.disabled = True
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def test_error_collection(self):
+
+        with open(os.path.join(self.base_dir, 'fixtures/error_collection.json')) as f:
+            collection = json.load(f)
+
+        for m in collection['manifests']:
+            man = m['manifest']
+            self.test_subject.validate(m['manifest'])
+
+            self.assertTrue(self.has_errors(), "Did not catch error on '{}'".format(m['label']))
+            self.clear_errors_and_warnings()
