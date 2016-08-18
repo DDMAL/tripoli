a2e86639c2f343d3d2f9bec9f434802e2aef6289
Alex Parmentier <a.g.parmentier@gmail.com>
2016-08-18 11:39:54 -0400

    Fixed: __version__ accessible from module level.
    import tripoli
    tripoli.__version__
    
    The above will now work.

--- tripoli/__init__.py
+++ tripoli/__init__.py
@@ -1,3 +1,4 @@
 from .tripoli import IIIFValidator, AnnotationValidator, \
-    ManifestValidator, CanvasValidator, SequenceValidator, ImageContentValidator
+    ManifestValidator, CanvasValidator, SequenceValidator, ImageContentValidator,\
+    __version__
 from .resource_validators.base_validator import BaseValidator
