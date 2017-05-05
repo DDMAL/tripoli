db89efa8d62070eeba71cc0634a4300f33a36123
Alex Parmentier <a.g.parmentier@gmail.com>
2017-05-05 09:10:57 -0400

    Tests: Added new test for debug mode.

--- tests/test_iiif_validator.py
+++ tests/test_iiif_validator.py
@@ -5,7 +5,6 @@ except ImportError:
 
 import os
 
-
 from .validator_testing_tools import ValidatorTestingTools
 from tripoli import IIIFValidator
 
@@ -21,6 +20,13 @@ class TestIIIFValidator(ValidatorTestingTools):
 
         self.base_dir = os.path.dirname(os.path.realpath(__file__))
 
+        with open(os.path.join(self.base_dir, 'fixtures/valid_manifest')) as f:
+            self.valid_manifest = json.load(f)
+
+        with open(os.path.join(self.base_dir, 'fixtures/error_collection.json')) as f:
+            self.error_collection = json.load(f)
+            self.man_with_warnings_and_errors = self.error_collection['manifests'][-1]['manifest']
+
     def test_valid_manifest(self):
         """Test that a valid manifest raises no errors."""
         with open(os.path.join(self.base_dir, 'fixtures/valid_manifest')) as f:
@@ -34,3 +40,17 @@ class TestIIIFValidator(ValidatorTestingTools):
             self.test_subject.validate(f.read())
         self.assertFalse(self.has_errors())
 
+    def test_debug_setting(self):
+        """Test that the debug setting works."""
+        iv = IIIFValidator(debug=True)
+        iv.validate(self.man_with_warnings_and_errors)
+        self.assertTrue(bool(iv.errors[0]._tb))
+
+        iv = IIIFValidator(debug=False)
+        iv.validate(self.man_with_warnings_and_errors)
+        self.assertFalse(bool(iv.errors[0]._tb))
+
+    def test_collect_warnings_setting(self):
+        """Test that collect_warnings setting works."""
+        iv = IIIFValidator(collect_warnings=False)
+        iv.validate(self.man_with_warnings_and_errors)
